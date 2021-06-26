969416eb924744e289a57e128c09b7ac9b85cb4f
Your Name <you@example.com>
2021-06-27 00:05:09 +0530

    middleware updated

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
-
+const cookieParser = require('cookie-parser')
 module.exports = async function isAuthenticated(req, res, next) {
-    const token = req.headers["authorization"].split(" ")[1];
-
+    //const token = req.headers["authorization"].split(" ")[1];
+    const token = req.cookies.jwt;
     jwt.verify(token, "secret", (err, user) => {
         if (err) {
             console.log(err)
