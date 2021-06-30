12bfbef387cf99aa5c094c452f6fd316cdad56b1
Your Name <you@example.com>
2021-06-30 15:26:34 +0530

    middleware updated

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,26 +1,29 @@
 const jwt = require('jsonwebtoken')
 //const cookieParser = require('cookie-parser')
-module.exports = async function isAuthenticated(req, res, next) {
-        const token = req.headers["authorization"].split(" ")[1];
-        //const token = req.cookies.jwt;
+var userEmail;
+const isAuthenticated=(req, res, next)=> {
+        let token;
+        const check = req.headers.authorization;
+        if(!check){
+            res.status(201).json({ message: "Please login" })
+        }else{
+            token = req.headers["authorization"].split(" ")[1];
+            //const token = req.cookies.jwt;
 
-    //check token web exit 
-    if(token){
-        jwt.verify(token,'secret', (err,data) => {
-            if(err){
-                res.status(401).json({ message: "Unauthorized client" }); 
-            }
-            else{
-                userEmail = data.email;
-                console.log(userEmail);         
-                next()
-            }
-        })
-    }
-    else{
-        // res.status(401).json({ message: "Unauthorized client" }); 
-        res.status(205).json({ message: "Please login" })
-    }
-    
+             //check token web exit 
+            jwt.verify(token,'secret', (err,data) => {
+                if(err){
+                    res.status(401).json({ message: "You are not Unauthorized to go further" }); 
+                }
+                else{
+                    userEmail = data.email;
+                    userType = data.userType;
+                    console.log(userEmail);         
+                    next()
+                }
+            })
+        }
+       
 }
 
+module.exports = { isAuthenticated, userEmail};
